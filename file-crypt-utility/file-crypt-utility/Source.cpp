21d29bbdc9854e35f5d602b114229c0735c68501
Suako <hasdoty@gmail.com>
2023-07-29 18:07:30 +0100

    Updated Source.cpp
    - Added file class
    - Encrypt File method, currently reads files only
    - Get file size method added

--- file-crypt-utility/file-crypt-utility/Source.cpp
+++ file-crypt-utility/file-crypt-utility/Source.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <filesystem>
+#include <array>
 
 std::int8_t startup()
 {
@@ -31,10 +35,79 @@ std::int8_t startup()
 	}
 }
 
-int main()
+class file
 {
-	std::int8_t _selected_option{ startup() };
+public:
+	file(std::filesystem::path file_path)
+	{
+		_file_path = file_path;
+	}
 
+	// Method to encrypt a file
+	void encrypt_file()
+	{
+		const auto _file_size{ get_file_size(_file_path) };
+		std::ifstream _file_stream{ _file_path, std::ios::binary };
+		const auto _base_path{ _file_path.parent_path() / _file_path.stem() };
+		std::filesystem::path _new_path{ _base_path.string() + "2" + _file_path.extension().string()};
+		std::ofstream _output_file{ _new_path, std::ios::binary };
+		constexpr int _buffer_size = 4096;
+		std::array<char, _buffer_size> _buffer{};
+		std::uintmax_t _total_batches{ (_file_size / 4096) };
+
+		if (_file_stream.is_open() == false && _output_file.is_open() == false)
+		{
+			std::cerr << "Failed to open file D:" << std::endl;
+			return;
+		}
+		else 
+		{
+			_file_stream.seekg(0);
+
+			for (int i = 0; i < _total_batches; i++)
+			{
+				_file_stream.read(_buffer.data(), _buffer.size());
+				_output_file.write(_buffer.data(), _buffer.size());
+			}
+
+			auto _remaining_size{ _file_size % _buffer_size };
+
+			_file_stream.read(_buffer.data(), _remaining_size);
+			_output_file.write(_buffer.data(), _remaining_size);
+		}
+	}
+
+	// Method to decrypt a file
+	void decrypt_file()
+	{
+
+	}
+
+private:
+	std::filesystem::path _file_path;
+	int64_t _current_character;
+	uintmax_t _file_size;
+
+	uintmax_t get_file_size(std::filesystem::path path)
+	{
+		return std::filesystem::file_size(path);
+	}
+
+	void read_file_section()
+	{
+
+	}
+};
+
+int main(int argc, char** argv)
+{
+	//std::int8_t _selected_option{ startup() };
+
+	std::filesystem::path _path("C:\\Users\\yusuf\\Downloads\\asd.zip");
+
+	file _newFile = file(_path);
+
+	_newFile.encrypt_file();
 
 	return 0;
 }
